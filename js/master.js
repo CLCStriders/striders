e846f06cb08d5d25e0b922195affe16902a699ab
Benny <ChelseaStats@users.noreply.github.com>
2019-12-16 23:35:56 +0000

    Update master.js

--- js/master.js
+++ js/master.js
@@ -2,7 +2,7 @@
 $(document).ready(function() { $('table').addClass('table table-bordered table-striped table-hover table-sm .table-responsive'); });
 
 // add datatables to bootstrap
-$(document).ready(function() { $('#output_data').DataTable(); } );
+$(document).ready(function() { $('#output_data_table').DataTable(); } );
 
 // homepage carousel
 $(document).ready(function() { $('#gallery').carousel({ interval: false}); });
