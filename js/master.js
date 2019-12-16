6d76dcfe1a61276b1fed5d2cb006ef65b0865289
Benny <ChelseaStats@users.noreply.github.com>
2019-12-16 20:46:41 +0000

    Update master.js

--- js/master.js
+++ js/master.js
@@ -1,3 +1,4 @@
-$(document).ready(function() {
-    $('table').addClass('table table-bordered table-striped table-hover table-sm .table-responsive');
-});
+// add bootstrap table to all tables
+$(document).ready(function() { $('table').addClass('table table-bordered table-striped table-hover table-sm .table-responsive'); });
+// homepage carousel
+$(document).ready(function() { $('#gallery').carousel({ interval: false}); });
