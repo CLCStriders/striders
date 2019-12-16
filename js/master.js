227e90be4dbaddf044706e7acf5595267d2acf8f
Benny <ChelseaStats@users.noreply.github.com>
2019-12-16 23:34:41 +0000

    Update master.js

--- js/master.js
+++ js/master.js
@@ -13,4 +13,8 @@ var taglines=[
 "CLC Striders have <a href='/pages/training'>training runs</a> on Monday and Wednesday, and Friday during track season.",
 "CLC Striders host the <a href='/pages/staverton-10'>Staverton 10 mile</a> in January every year. Entries are now open for 2020.",	
 ]
-document.querySelector('#tagline').innerHTML = taglines[ Math.floor(Math.random()*taglines.length) ];
+
+var selection = document.querySelector('#tagline') !== null;
+if(selection) {
+  selection.innerHTML = taglines[ Math.floor(Math.random()*taglines.length) ];
+}
