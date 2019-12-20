d8fc3368b7c08362e2f1b5d325db0895f4fbdde6
Mat B <Mat.Benfield@hesa.ac.uk>
2019-12-20 23:28:11 +0000

    chain things.

--- assets/js/data.js
+++ assets/js/data.js
@@ -4,69 +4,61 @@
 ---
 
 $(document).ready(function () {
- $('#load_data').ready(function(){
+ $('#load_data').ready(function() {
      $.ajax({
-   url: "{{ page.source }}?v={{ site.cache }}",
-   dataType:"text",
-   success:function(data)
-   {
-    var row_data = data.split(/\r?\n|\r/);
-    var table_data = '<table id="output_table_data" class="display table table-bordered table-striped table-hover table-sm .table-responsive">';
-    for(var count = 0; count<row_data.length; count++)
-    {
-      if( count === 0 ) {
-       table_data +='<thead>';
-        var cell_data = row_data[count].split(",");
-         table_data += '<tr>';
-         for(var cell_count=0; cell_count<cell_data.length; cell_count++)
-         {
-          if(count === 0)
-          {
-           table_data += '<th data-field="'+cell_data[cell_count]+'">'+cell_data[cell_count]+'</th>';
-          }
-         }
-         table_data += '</tr>';
-       table_data +='</thead><tbody>';   
-      }
-      else {
-         var cell_data = row_data[count].split(",");
-         table_data += '<tr>';
-         for(var cell_count=0; cell_count<cell_data.length; cell_count++)
-         {
-          if(count === 0)
-          {
-           table_data += '<th>'+cell_data[cell_count]+'</th>';
-          }
-          else
-          {
-            if(cell_data == "") 
-            {
-              table_data += '';
-            }
-            else {    
-              table_data += '<td>'+cell_data[cell_count]+'</td>';
-            }
-          }
-         }
-         table_data += '</tr>';
-      }
-    }
-    table_data += '</tbody></table>';
-     
-    $('#output_table_div').html(table_data);
-   }
-  });
- }),
-    $('#output_table_div').DataTable({
-        columns: [
-            { data: "Type" },
-            { data: "Gender" },
-            { data: "Distance" },
-            { data: "Age Group" },
-            { data: "Holder" },
-            { data: "Time" },
-            { data: "Venue" },
-            { data: "Date" }
-        ]
-    })
-});
+         url: "{{ page.source }}?v={{ site.cache }}",
+         dataType: "text",
+         success: function(data) {
+             var row_data = data.split(/\r?\n|\r/);
+             var table_data =
+                 '<table id="output_table_data" class="display table table-bordered table-striped table-hover table-sm table-responsive">';
+             for (var count = 0; count < row_data.length; count++) {
+                 if (count === 0) {
+                     table_data += '<thead>';
+                     var cell_data = row_data[count].split(",");
+                     table_data += '<tr>';
+                     for (var cell_count = 0; cell_count < cell_data.length; cell_count++) {
+                         if (count === 0) {
+                             table_data += '<th data-field="' +
+                                 cell_data[cell_count] +
+                                 '">' +
+                                 cell_data[cell_count] +
+                                 '</th>';
+                         }
+                     }
+                     table_data += '</tr>';
+                     table_data += '</thead><tbody>';
+                 } else {
+                     var cell_data = row_data[count].split(",");
+                     table_data += '<tr>';
+                     for (var cell_count = 0; cell_count < cell_data.length; cell_count++) {
+                         if (count === 0) {
+                             table_data += '<th>' + cell_data[cell_count] + '</th>';
+                         } else {
+                             if (cell_data == "") {
+                                 table_data += '';
+                             } else {
+                                 table_data += '<td>' + cell_data[cell_count] + '</td>';
+                             }
+                         }
+                     }
+                     table_data += '</tr>';
+                 }
+             }
+             table_data += '</tbody></table>';
+
+             $('#output_table_div').html(table_data).DataTable({
+                 columns: [
+                     { data: "Type" },
+                     { data: "Gender" },
+                     { data: "Distance" },
+                     { data: "Age Group" },
+                     { data: "Holder" },
+                     { data: "Time" },
+                     { data: "Venue" },
+                     { data: "Date" }
+                 ]
+             });
+
+             console.log(url);
+         });
