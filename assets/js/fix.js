cef5314d08f7f03f40fcd87cceecf9cac2b74138
Mat B <Mat.Benfield@hesa.ac.uk>
2019-12-21 17:16:44 +0000

    fixing

--- assets/js/fix.js
+++ assets/js/fix.js
@@ -6,8 +6,8 @@
 $('.committee').DataTable({ "paging": false, "info": false });
 
 
-var myData = '{{ site.data.fixtures | jsonify }}';
-$('#site_data_fixtures').DataTable({ 
+var myData = {{ site.data.fixtures | jsonify }};
+$("#site_data_fixtures").DataTable({ 
 Data : myData,
 columns: [
         { data: "Data" },
