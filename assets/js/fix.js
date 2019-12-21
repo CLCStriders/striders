2ad324bde6bed7c2f8be37481a50116954b5de41
Mat B <Mat.Benfield@hesa.ac.uk>
2019-12-21 17:49:34 +0000

    fixtures attempt using
    https://stackoverflow.com/questions/34287402/datatables-cannot-read-property-length-of-undefined

--- assets/js/fix.js
+++ assets/js/fix.js
@@ -2,19 +2,30 @@
 # Front matter comment to ensure Jekyll properly reads file.
 ---
 
-
 $('.committee').DataTable({ "paging": false, "info": false });
 
 
-var myData = {{ site.data.fixtures | jsonify }};
-$("#site_data_fixtures").DataTable({ 
-Data : myData,
-columns: [
-        { data: "Date" },
-        { data: "Race" },
-        { data: "Distance" },
-        { data: "Terrain" }
-    ]
+//var myData = '{{ site.data.fixtures | jsonify }}';
+//$('#site_data_fixtures').DataTable({ 
+//Data : myData,
+//columns: [
+//        { data: "Data" },
+//        { data: "Race" },
+//        { data: "Distance" },
+//        { data: "Terrain" }
+//    ]
+//});
+
+var fixData = ["Rows": "{{ site.data.fixtures | jsonify }} "];
+$(document).ready(function () {
+    $("#site_data_fixtures").DataTable({
+        "ajax": fixData,
+        "dataSrc": "Rows",
+        "columns": [
+            { data: "Date" },
+            { data: "Race" },
+            { data: "Distance" },
+            { data: "Terrain" }
+        ]
+    });
 });
-
-
